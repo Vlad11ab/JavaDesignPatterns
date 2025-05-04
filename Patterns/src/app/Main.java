7d687b568678d6e72533e1931a50b2170b0d5cd2
Vlad11ab <breazuvlad11@gmail.com>
2025-05-04 21:02:36 +0300

    tried some examples with builder design pattern

--- Patterns/src/app/Main.java
+++ Patterns/src/app/Main.java
@@ -1,6 +1,9 @@
 package app;
 
 
+import app.builder.builders.Builder;
+import app.builder2.User;
+import app.builder3.Order;
 import app.singleton.view.View;
 import app.singleton.view.View2;
 
@@ -8,5 +11,10 @@ public class Main {
     public static void main(String[] args) {
 
 
+       Order order = Order.builder().customerId(10)
+               .amount(10)
+               .id(10)
+               .shippingAddress("sasd")
+               .build();
     }
 }
